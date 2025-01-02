8aebba26ca94ce101b68b6aa6db39a6d8e68951d
Fabián Paucar <faibol2000@gmail.com>
2025-01-02 14:34:48 -0500

    Calculo del Área de un triángulo

---
+++ Documents/NetBeansProjects/AreaRectangulo/src/area/Rectangulo.java
@@ -0,0 +1,20 @@
+
+package area;
+
+public class Rectangulo {
+    private int vbase, valtura, area;
+    
+    public Rectangulo(int base, int altura){
+        this.vbase = base;
+        this.valtura = altura;
+    }
+        
+    private void calArea(){
+        area = vbase * valtura;
+    }
+    
+    public void impArea(){
+        calArea();
+        System.out.println("El area del triángulo es: " + area);
+    } 
+}
